e303f59c24cc17f6b13ffb79d1ed2e0692de5c71
NadineTsianta <k.tsianta@rocketmail.com>
2024-02-19 23:29:25 +0000

    moved readme template in the generate Markdown file

--- starter/index.js
+++ starter/index.js
@@ -4,53 +4,6 @@ const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 
-const readme = (response) => `# ${response.title}
-
-## Description
-
-${response.description}
-
-## Table of Contents
-
-- ${[response.installation](#response.installation)}
-- ${[response.usage](#response.usage)}
-- ${[response.contribution](#response.contribution)}
-- ${[response.license](#response.license)}
-- ${[response.badge](#response.badge)}
-- ${[response.tests](#response.tests)}
-- Questions
-
-
-
-## Installation
-
- ${[response.installation]}
-
-## Usage
- ${[response.usage]}
-
-## Contribution Guidelines
-
- ${[response.contribution]}
-
-## License
- ${[response.license]}
-
-
-## Badge
-
- ![Badge](https://img.shields.io/badge/&{badge}-blue)
-
-Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
-
-
-## Tests
- ${[response.tests]}
- 
- ## Questions
-   ${[Github Username](#response.username)}
-   ${[Contact me](#response.email)}
- `
 
 // array of questions for user
 const questions = [{
@@ -113,18 +66,20 @@ const questions = [{
 
 inquirer
 .promt(questions)
+
 .then((response)=> {
     console.log(response);
 
-    fs.writeFile('README.md', readme(response), err =>{
+})
+
+
+// function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile('README.md', generateMarkdown(response), err =>{
         if (err) {
             console.log(err);
         }
     })
-})
-
-// function to write README file
-function writeToFile(fileName, data) {
 }
 
 // function to initialize program
--- starter/utils/generateMarkdown.js
+++ starter/utils/generateMarkdown.js
@@ -1,8 +1,52 @@
 // function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(response) {
+  return `# ${response.title}
 
-`;
+  ## Description
+  
+  ${response.description}
+  
+  ## Table of Contents
+  
+  - ${[response.installation]}(#installation)
+  - ${[response.usage]}(#usage)
+  - ${[response.contribution]}(#contribution)
+  - ${[response.license]}(#license)
+  - ${[response.badge]}(#badge)
+  - ${[response.tests]}(#tests)
+  - Questions
+  
+  
+  
+  ## Installation
+  
+   ${[response.installation]}
+  
+  ## Usage
+   ${[response.usage]}
+  
+  ## Contribution Guidelines
+  
+   ${[response.contribution]}
+  
+  ## License
+   ${[response.license]}
+  
+  
+  ## Badge
+  
+   ![Badge](https://img.shields.io/badge/${badge}-blue)
+  
+  Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+  
+  
+  ## Tests
+   ${[response.tests]}
+   
+   ## Questions
+     Github_Username: ${response.username}
+     Contact_me: ${response.email}
+   `;
 }
 
 module.exports = generateMarkdown;
